1964250ea8f78d12d4fc6bc82f6664bcc515898f
Yuhan Zhao <yuhanzhao@student.mitt.ca>
2023-06-19 13:04:45 -0500

    update

--- Object-Oriented-Programming-Fundamentals/Program.cs
+++ Object-Oriented-Programming-Fundamentals/Program.cs
@@ -1,6 +1,6 @@
-﻿VendingMachine vendingMachine = new VendingMachine();
+﻿VendingMachine vendingMachine = new VendingMachine("hahaha");
 Console.WriteLine(vendingMachine.SerialNumber);
-VendingMachine vendingMachine2 = new VendingMachine();
+VendingMachine vendingMachine2 = new VendingMachine("xixixi");
 Console.WriteLine(vendingMachine2.SerialNumber);
 class VendingMachine
 {
@@ -8,10 +8,25 @@ class VendingMachine
     public int SerialNumber { get { return _serialNumber; } }
     private string? _barcode;
     public string? Barcode { get { return _barcode; } }
+
     private Dictionary<int, int> MoneyFloat { get; }
     private Dictionary<Product, int> Inventory { get; }
-    public VendingMachine()
+    public VendingMachine(string barcode)
     {
+        try
+        {
+            if (!string.IsNullOrEmpty(barcode))
+            {
+                _barcode = barcode;
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(barcode), "Barcode can not be empty.");
+            }
+        } catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
         _serialNumber++;
         MoneyFloat = new Dictionary<int, int>
         {
@@ -25,13 +40,27 @@ class VendingMachine
     }
     public void StockItem(Product product, int quantity)
     {
-        if (Inventory.ContainsKey(product))
+        try
         {
-            Inventory[product] += quantity;
+            if (quantity >= 0) 
+            {
+                if (Inventory.ContainsKey(product))
+                {
+                    Inventory[product] += quantity;
+                }
+                else
+                {
+                    Inventory.Add(product, quantity);
+                }
+            }
+            else 
+            {
+                throw new ArgumentOutOfRangeException(nameof(quantity), "Quantity must be equal or greater than 0.");
+            }
         }
-        else
+        catch(Exception e)
         {
-            Inventory.Add(product, quantity);
+            Console.WriteLine(e.Message);
         }
         Console.WriteLine($"Stocked Items: Name: {product.Name}, Code: {product.Code}, Price: ${product.Price}, Quantity: {Inventory[product]}");
     }
@@ -50,71 +79,85 @@ class VendingMachine
 
     public string VendItem(string code, List<int> money)
     {
-        // set a null temp product to find if the product exist
-        Product tempProduct = null;
-        foreach (KeyValuePair<Product, int> item in Inventory)
+        try
         {
-            if (item.Key.Code == code)
+            if (string.IsNullOrEmpty(code) && money.Count > 0)
             {
-                tempProduct = item.Key;
-                break;
-            }
-        }
-        if (tempProduct == null)
-        {
-            return $"Error, no item with code {code}";
-        } 
-        else if (Inventory[tempProduct] == 0)
-        {
-            return "Error: Item is out of stock";
-        } 
-        else
-        {
-            int total = 0;
-            foreach (int amount in money)
-            {
-                total += amount;
-            }
-            if (total < tempProduct.Price)
-            {
-                return "Error: insufficient money provided";
-            }
-            else if (total == tempProduct.Price)
-            {
-                return $"Please enjoy your '{tempProduct.Name}'. No change required.";
+                // set a null temp product to find if the product exist
+                Product tempProduct = null;
+                foreach (KeyValuePair<Product, int> item in Inventory)
+                {
+                    if (item.Key.Code == code)
+                    {
+                        tempProduct = item.Key;
+                        break;
+                    }
+                }
+                if (tempProduct == null)
+                {
+                    return $"Error, no item with code {code}";
+                }
+                else if (Inventory[tempProduct] == 0)
+                {
+                    return "Error: Item is out of stock";
+                }
+                else
+                {
+                    int total = 0;
+                    foreach (int amount in money)
+                    {
+                        total += amount;
+                    }
+                    if (total < tempProduct.Price)
+                    {
+                        return "Error: insufficient money provided";
+                    }
+                    else if (total == tempProduct.Price)
+                    {
+                        return $"Please enjoy your '{tempProduct.Name}'. No change required.";
+                    }
+                    else
+                    {
+                        int leftAmount = total - tempProduct.Price;
+                        Dictionary<int, int> returnMoneyList = new Dictionary<int, int>();
+                        List<int> allMoney = new List<int>(MoneyFloat.Keys);
+                        // Sort from big to small
+                        allMoney.Sort();
+                        allMoney.Reverse();
+                        // allMoney.Sort((a, b) => b.CompareTo(a));
+                        foreach (int moneyList in allMoney)
+                        {
+                            int count = MoneyFloat[moneyList];
+                            if (leftAmount >= moneyList && count > 0)
+                            {
+                                int dispense = leftAmount / moneyList;
+                                if (count < dispense)
+                                {
+                                    dispense = count;
+                                }
+                                MoneyFloat[moneyList] -= dispense;
+                                leftAmount -= moneyList * dispense;
+                                returnMoneyList.Add(moneyList, dispense);
+                            }
+                        }
+                        if (leftAmount > 0)
+                        {
+                            return "Unable to dispense change.";
+                        }
+                        Inventory[tempProduct]--;
+                        string change = printChange(returnMoneyList);
+                        return $"Please enjoy your '{tempProduct.Name}' and take your change of {change}";
+                    }
+                }
             }
             else
             {
-                int leftAmount = total - tempProduct.Price;
-                Dictionary<int, int> returnMoneyList = new Dictionary<int, int>();
-                List<int> allMoney = new List<int>(MoneyFloat.Keys);
-                // Sort from big to small
-                allMoney.Sort();
-                allMoney.Reverse();
-                // allMoney.Sort((a, b) => b.CompareTo(a));
-                foreach (int moneyList in allMoney)
-                {
-                    int count = MoneyFloat[moneyList];
-                    if (leftAmount >= moneyList && count > 0)
-                    {
-                        int dispense = leftAmount / moneyList;
-                        if (count < dispense)
-                        {
-                            dispense = count;
-                        }
-                        MoneyFloat[moneyList] -= dispense;
-                        leftAmount -= moneyList * dispense;
-                        returnMoneyList.Add(moneyList, dispense);
-                    }
-                }
-                if (leftAmount > 0)
-                {
-                    return "Unable to dispense change.";
-                }
-                Inventory[tempProduct]--;
-                string change = printChange(returnMoneyList);
-                return $"Please enjoy your '{tempProduct.Name}' and take your change of {change}";
+                throw new Exception("Code or Money is invalid.");
             }
+        }
+        catch (Exception ex) 
+        { 
+            throw new Exception(ex.Message); 
         }
     }
     private string printChange(Dictionary<int, int> changeReturned)
@@ -137,6 +180,25 @@ class Product
     public string Code { get { return _code; } }
     public Product(string name, int price, string code)
     {
+        try
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name), "Name can not be empty.");
+            }
+            if (string.IsNullOrEmpty(code))
+            {
+                throw new ArgumentNullException(nameof(code), "Code can not be empty.");
+            }
+            if (price < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(price), "Price has to be 0 or greater than 0.");
+            }
+        }
+        catch (Exception e)
+        {
+            throw new ArgumentException(e.Message);
+        }
         _name = name;
         _price = price;
         _code = code;
